dbb974aa85ab2cb177aa87f3f26399390bcab3ef
matthewsotoudeh <matthewsot@outlook.com>
2014-11-27 15:21:07 -0800

    commented up ClauseSyntaxParse

---
+++ SharpSwift/SharpSwift/Converters/ClauseSyntaxParser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SharpSwift.Converters
+{
+    partial class ConvertToSwift
+    {
+        /// <summary>
+        /// Converts an equals value clause to Swift
+        /// </summary>
+        /// <example>= something_else()</example>
+        /// <param name="clause">The clause to convert</param>
+        /// <returns>The converted Swift clause</returns>
+        [ParsesType(typeof(EqualsValueClauseSyntax))]
+        public static string EqualsValueClause(EqualsValueClauseSyntax clause)
+        {
+            return clause.EqualsToken + " " + SyntaxNode(clause.Value).Trim();
+        }
+    }
+}
